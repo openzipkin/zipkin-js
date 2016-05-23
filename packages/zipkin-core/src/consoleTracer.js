0157d09ad346ca6c833d31b7b070fcab7dd10467
eirslett <eirik.sletteberg@finn.no>
2016-05-23 14:46:22 +0200

    log parentId in consoleTracer

--- packages/zipkin-core/src/consoleTracer.js
+++ packages/zipkin-core/src/consoleTracer.js
@@ -3,7 +3,7 @@ module.exports = {
     /* eslint-disable no-console */
     const id = rec.traceId;
     console.log(
-      `Record at (spanId=${id.spanId}, traceId=${id.traceId}): ${rec.annotation.toString()}`
+      `Record at (spanId=${id.spanId}, parentId=${id.parentId}, traceId=${id.traceId}): ${rec.annotation.toString()}`
     );
   },
   toString() {
