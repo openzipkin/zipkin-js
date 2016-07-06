470043e3e2486e85202a383ec05352e102a7f5e0
Jacob Quatier <jquatier@gmail.com>
2016-07-06 15:31:41 -0700

    Restify middleware package

---
+++ packages/zipkin-instrumentation-restify/src/restifyMiddleware.js
@@ -0,0 +1,96 @@
+const {
+  Annotation,
+  HttpHeaders: Header,
+  option: {Some, None},
+  TraceId
+} = require('zipkin');
+const url = require('url');
+
+function containsRequiredHeaders(req) {
+  return req.header(Header.TraceId) !== undefined &&
+    req.header(Header.SpanId) !== undefined;
+}
+
+function stringToBoolean(str) {
+  return str === '1';
+}
+
+function stringToIntOption(str) {
+  try {
+    return new Some(parseInt(str));
+  } catch (err) {
+    return None;
+  }
+}
+
+module.exports = function restifyMiddleware({tracer, serviceName = 'unknown', port = 0}) {
+  return function zipkinRestifyMiddleware(req, res, next) {
+    tracer.scoped(() => {
+      function readHeader(header) {
+        const val = req.header(header);
+        if (val != null) {
+          return new Some(val);
+        } else {
+          return None;
+        }
+      }
+
+      if (containsRequiredHeaders(req)) {
+        const spanId = readHeader(Header.SpanId);
+        spanId.ifPresent(sid => {
+          const traceId = readHeader(Header.TraceId);
+          const parentSpanId = readHeader(Header.ParentSpanId);
+          const sampled = readHeader(Header.Sampled);
+          const flags = readHeader(Header.Flags).flatMap(stringToIntOption).getOrElse(0);
+          const id = new TraceId({
+            traceId,
+            parentId: parentSpanId,
+            spanId: sid,
+            sampled: sampled.map(stringToBoolean),
+            flags
+          });
+          tracer.setId(id);
+        });
+      } else {
+        tracer.setId(tracer.createRootId());
+        if (req.header(Header.Flags)) {
+          const currentId = tracer.id;
+          const idWithFlags = new TraceId({
+            traceId: currentId.traceId,
+            parentId: currentId.parentId,
+            spanId: currentId.spanId,
+            sampled: currentId.sampled,
+            flags: readHeader(Header.Flags)
+          });
+          tracer.setId(idWithFlags);
+        }
+      }
+
+      const id = tracer.id;
+
+      tracer.recordServiceName(serviceName);
+      tracer.recordRpc(req.method);
+      tracer.recordBinary('http.url', url.format({
+        protocol: req.isSecure() ? 'https' : 'http',
+        host: req.header('host'),
+        pathname: req.path()
+      }));
+      tracer.recordAnnotation(new Annotation.ServerRecv());
+      tracer.recordAnnotation(new Annotation.LocalAddr({port}));
+
+      if (id.flags !== 0 && id.flags != null) {
+        tracer.recordBinary(Header.Flags, id.flags.toString());
+      }
+
+      res.on('finish', () => {
+        tracer.scoped(() => {
+          tracer.setId(id);
+          tracer.recordBinary('http.status_code', res.statusCode.toString());
+          tracer.recordAnnotation(new Annotation.ServerSend());
+        });
+      });
+
+      next();
+    });
+  };
+};
---
+++ packages/zipkin-instrumentation-restify/test/integrationTest.js
@@ -0,0 +1,85 @@
+const sinon = require('sinon');
+const {Tracer, ExplicitContext} = require('zipkin');
+const fetch = require('node-fetch');
+const restify = require('restify');
+const middleware = require('../src/restifyMiddleware');
+
+describe('restify middleware - integration test', () => {
+  it('should receive trace info from the client', done => {
+    const record = sinon.spy();
+    const recorder = {record};
+    const ctxImpl = new ExplicitContext();
+    const tracer = new Tracer({recorder, ctxImpl});
+
+    ctxImpl.scoped(() => {
+      const app = restify.createServer();
+      app.use(middleware({
+        tracer,
+        serviceName: 'service-a'
+      }));
+      app.post('/foo', (req, res, next) => {
+        // Use setTimeout to test that the trace context is propagated into the callback
+        const ctx = ctxImpl.getContext();
+        setTimeout(() => {
+          ctxImpl.letContext(ctx, () => {
+            tracer.recordBinary('message', 'hello from within app');
+            res.send(202, {status: 'OK'});
+          });
+        }, 10);
+        return next();
+      });
+      const server = app.listen(0, () => {
+        const port = server.address().port;
+        const url = `http://127.0.0.1:${port}/foo`;
+        fetch(url, {
+          method: 'post',
+          headers: {
+            'X-B3-TraceId': 'aaa',
+            'X-B3-SpanId': 'bbb',
+            'X-B3-Flags': '1'
+          }
+        }).then(res => res.json()).then(() => {
+          server.close();
+
+          const annotations = record.args.map(args => args[0]);
+
+          annotations.forEach(ann => expect(ann.traceId.traceId).to.equal('aaa'));
+          annotations.forEach(ann => expect(ann.traceId.spanId).to.equal('bbb'));
+
+          expect(annotations[0].annotation.annotationType).to.equal('ServiceName');
+          expect(annotations[0].annotation.serviceName).to.equal('service-a');
+
+          expect(annotations[1].annotation.annotationType).to.equal('Rpc');
+          expect(annotations[1].annotation.name).to.equal('POST');
+
+          expect(annotations[2].annotation.annotationType).to.equal('BinaryAnnotation');
+          expect(annotations[2].annotation.key).to.equal('http.url');
+          expect(annotations[2].annotation.value).to.equal(url);
+
+          expect(annotations[3].annotation.annotationType).to.equal('ServerRecv');
+
+          expect(annotations[4].annotation.annotationType).to.equal('LocalAddr');
+
+          expect(annotations[5].annotation.annotationType).to.equal('BinaryAnnotation');
+          expect(annotations[5].annotation.key).to.equal('X-B3-Flags');
+          expect(annotations[5].annotation.value).to.equal('1');
+
+          expect(annotations[6].annotation.annotationType).to.equal('BinaryAnnotation');
+          expect(annotations[6].annotation.key).to.equal('message');
+          expect(annotations[6].annotation.value).to.equal('hello from within app');
+
+          expect(annotations[7].annotation.annotationType).to.equal('BinaryAnnotation');
+          expect(annotations[7].annotation.key).to.equal('http.status_code');
+          expect(annotations[7].annotation.value).to.equal('202');
+
+          expect(annotations[8].annotation.annotationType).to.equal('ServerSend');
+          done();
+        })
+        .catch(err => {
+          server.close();
+          done(err);
+        });
+      });
+    });
+  });
+});
