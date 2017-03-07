e512f730f4ffe02c30f69b3d37c3ba12495f46de
Momont, Matt (GE Global Research) <Matt.Momont@ge.com>
2017-03-07 11:44:43 -0800

    Add create trace id test case

--- packages/zipkin-instrumentation-express/test/expressMiddlewareIntegrationTest.js
+++ packages/zipkin-instrumentation-express/test/expressMiddlewareIntegrationTest.js
@@ -5,7 +5,80 @@ const express = require('express');
 const middleware = require('../src/expressMiddleware');
 
 describe('express middleware - integration test', () => {
-  it.skip('should create traceId', () => {});
+  it('should create traceId', done => {
+    const record = sinon.spy();
+    const recorder = {record};
+    const ctxImpl = new ExplicitContext();
+    const tracer = new Tracer({recorder, ctxImpl});
+
+    ctxImpl.scoped(() => {
+      const app = express();
+      app.use(middleware({
+        tracer,
+        serviceName: 'service-a'
+      }));
+      app.post('/foo', (req, res) => {
+        // Use setTimeout to test that the trace context is propagated into the callback
+        const ctx = ctxImpl.getContext();
+        setTimeout(() => {
+          ctxImpl.letContext(ctx, () => {
+            tracer.recordBinary('message', 'hello from within app');
+            res.status(202).json({status: 'OK'});
+          });
+        }, 10);
+      });
+      const server = app.listen(0, () => {
+        const port = server.address().port;
+        const url = `http://127.0.0.1:${port}/foo`;
+        fetch(url, {
+          method: 'post'
+        }).then(res => res.json())
+          .then(() => {
+            server.close();
+
+            const annotations = record.args.map(args => args[0]);
+            const originalTraceId = annotations[0].traceId.traceId;
+            const originalSpanId = annotations[0].traceId.spanId;
+
+            annotations.forEach(ann => expect(ann.traceId.traceId)
+              .to.have.lengthOf(16).and
+              .to.equal(originalTraceId));
+            annotations.forEach(ann => expect(ann.traceId.spanId)
+              .to.have.lengthOf(16).and
+              .to.equal(originalSpanId));
+
+            expect(annotations[0].annotation.annotationType).to.equal('ServiceName');
+            expect(annotations[0].annotation.serviceName).to.equal('service-a');
+
+            expect(annotations[1].annotation.annotationType).to.equal('Rpc');
+            expect(annotations[1].annotation.name).to.equal('POST');
+
+            expect(annotations[2].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[2].annotation.key).to.equal('http.url');
+            expect(annotations[2].annotation.value).to.equal(url);
+
+            expect(annotations[3].annotation.annotationType).to.equal('ServerRecv');
+
+            expect(annotations[4].annotation.annotationType).to.equal('LocalAddr');
+
+            expect(annotations[5].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[5].annotation.key).to.equal('message');
+            expect(annotations[5].annotation.value).to.equal('hello from within app');
+
+            expect(annotations[6].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[6].annotation.key).to.equal('http.status_code');
+            expect(annotations[6].annotation.value).to.equal('202');
+
+            expect(annotations[7].annotation.annotationType).to.equal('ServerSend');
+            done();
+          })
+          .catch(err => {
+            server.close();
+            done(err);
+          });
+      });
+    });
+  });
   it('should receive trace info from the client', done => {
     const record = sinon.spy();
     const recorder = {record};
@@ -38,47 +111,48 @@ describe('express middleware - integration test', () => {
             'X-B3-SpanId': 'bbb',
             'X-B3-Flags': '1'
           }
-        }).then(res => res.json()).then(() => {
-          server.close();
+        }).then(res => res.json())
+          .then(() => {
+            server.close();
 
-          const annotations = record.args.map(args => args[0]);
+            const annotations = record.args.map(args => args[0]);
 
-          annotations.forEach(ann => expect(ann.traceId.traceId).to.equal('aaa'));
-          annotations.forEach(ann => expect(ann.traceId.spanId).to.equal('bbb'));
+            annotations.forEach(ann => expect(ann.traceId.traceId).to.equal('aaa'));
+            annotations.forEach(ann => expect(ann.traceId.spanId).to.equal('bbb'));
 
-          expect(annotations[0].annotation.annotationType).to.equal('ServiceName');
-          expect(annotations[0].annotation.serviceName).to.equal('service-a');
+            expect(annotations[0].annotation.annotationType).to.equal('ServiceName');
+            expect(annotations[0].annotation.serviceName).to.equal('service-a');
 
-          expect(annotations[1].annotation.annotationType).to.equal('Rpc');
-          expect(annotations[1].annotation.name).to.equal('POST');
+            expect(annotations[1].annotation.annotationType).to.equal('Rpc');
+            expect(annotations[1].annotation.name).to.equal('POST');
 
-          expect(annotations[2].annotation.annotationType).to.equal('BinaryAnnotation');
-          expect(annotations[2].annotation.key).to.equal('http.url');
-          expect(annotations[2].annotation.value).to.equal(url);
+            expect(annotations[2].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[2].annotation.key).to.equal('http.url');
+            expect(annotations[2].annotation.value).to.equal(url);
 
-          expect(annotations[3].annotation.annotationType).to.equal('ServerRecv');
+            expect(annotations[3].annotation.annotationType).to.equal('ServerRecv');
 
-          expect(annotations[4].annotation.annotationType).to.equal('LocalAddr');
+            expect(annotations[4].annotation.annotationType).to.equal('LocalAddr');
 
-          expect(annotations[5].annotation.annotationType).to.equal('BinaryAnnotation');
-          expect(annotations[5].annotation.key).to.equal('X-B3-Flags');
-          expect(annotations[5].annotation.value).to.equal('1');
+            expect(annotations[5].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[5].annotation.key).to.equal('X-B3-Flags');
+            expect(annotations[5].annotation.value).to.equal('1');
 
-          expect(annotations[6].annotation.annotationType).to.equal('BinaryAnnotation');
-          expect(annotations[6].annotation.key).to.equal('message');
-          expect(annotations[6].annotation.value).to.equal('hello from within app');
+            expect(annotations[6].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[6].annotation.key).to.equal('message');
+            expect(annotations[6].annotation.value).to.equal('hello from within app');
 
-          expect(annotations[7].annotation.annotationType).to.equal('BinaryAnnotation');
-          expect(annotations[7].annotation.key).to.equal('http.status_code');
-          expect(annotations[7].annotation.value).to.equal('202');
+            expect(annotations[7].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[7].annotation.key).to.equal('http.status_code');
+            expect(annotations[7].annotation.value).to.equal('202');
 
-          expect(annotations[8].annotation.annotationType).to.equal('ServerSend');
-          done();
-        })
-        .catch(err => {
-          server.close();
-          done(err);
-        });
+            expect(annotations[8].annotation.annotationType).to.equal('ServerSend');
+            done();
+          })
+          .catch(err => {
+            server.close();
+            done(err);
+          });
       });
     });
   });
@@ -110,20 +184,21 @@ describe('express middleware - integration test', () => {
         const url = `http://127.0.0.1:${port}/foo?abc=123`;
         fetch(url, {
           method: 'get'
-        }).then(res => res.json()).then(() => {
-          server.close();
+        }).then(res => res.json())
+          .then(() => {
+            server.close();
 
-          const annotations = record.args.map(args => args[0]);
+            const annotations = record.args.map(args => args[0]);
 
-          expect(annotations[2].annotation.annotationType).to.equal('BinaryAnnotation');
-          expect(annotations[2].annotation.key).to.equal('http.url');
-          expect(annotations[2].annotation.value).to.equal(url);
-          done();
-        })
-        .catch(err => {
-          server.close();
-          done(err);
-        });
+            expect(annotations[2].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[2].annotation.key).to.equal('http.url');
+            expect(annotations[2].annotation.value).to.equal(url);
+            done();
+          })
+          .catch(err => {
+            server.close();
+            done(err);
+          });
       });
     });
   });
@@ -161,18 +236,19 @@ describe('express middleware - integration test', () => {
             'X-B3-SpanId': '48485a3953bb6124',
             'X-B3-Flags': '1'
           }
-        }).then(res => res.json()).then(() => {
-          server.close();
+        }).then(res => res.json())
+          .then(() => {
+            server.close();
 
-          const annotations = record.args.map(args => args[0]);
+            const annotations = record.args.map(args => args[0]);
 
-          annotations.forEach(ann => expect(ann.traceId.traceId).to.equal(traceId));
-          done();
-        })
-        .catch(err => {
-          server.close();
-          done(err);
-        });
+            annotations.forEach(ann => expect(ann.traceId.traceId).to.equal(traceId));
+            done();
+          })
+          .catch(err => {
+            server.close();
+            done(err);
+          });
       });
     });
   });
