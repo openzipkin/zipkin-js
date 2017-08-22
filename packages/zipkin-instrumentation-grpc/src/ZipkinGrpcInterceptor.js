0b1ffec4b6be70e97fc1954dd70c7157e37e8612
Leonard Shi <leonard.shi.cs@gmail.com>
2017-08-22 10:04:21 +0800

    Refactor for GRPC server side.
    Signed-off-by: Leonard Shi <leonard.shi@vcube.co.jp>

--- packages/zipkin-instrumentation-grpc/src/ZipkinGrpcInterceptor.js
+++ packages/zipkin-instrumentation-grpc/src/ZipkinGrpcInterceptor.js
@@ -1,7 +1,9 @@
 const grpc = require('grpc');
 
 const {
-  Annotation
+  TraceId,
+  Annotation,
+  option: {Some}
 } = require('zipkin');
 
 class ZipkinGrpcInterceptor {
@@ -10,7 +12,8 @@ class ZipkinGrpcInterceptor {
     this.tracer = tracer;
   }
 
-  beforeGrpcCall({serviceName = 'unknown', remoteGrpcServiceName = 'unknown', xB3Sampled = '0', grpcMetadata}) {
+  // Call this right before the GRPC client is ready to call the GRPC server service.
+  beforeClientDoGrpcCall({serviceName = 'unknown', remoteGrpcServiceName = 'unknown', xB3Sampled = '0', grpcMetadata}) {
     return this.tracer.scoped(() => {
       let metadata;
 
@@ -41,7 +44,55 @@ class ZipkinGrpcInterceptor {
     });
   }
 
-  afterGrpcCall() {
+  // Call this at the first line upon the GRPC server received the call from the GRPC client side.
+  uponServerRecvGrpcCall({serviceName = 'unknown', grpcMetadataFromIncomingCtx}) {
+    if (!grpcMetadataFromIncomingCtx || !(grpcMetadataFromIncomingCtx instanceof grpc.Metadata)) {
+      throw new Error("The parameter 'grpcMetadataFromIncomingCtx' must be instance of 'grpc.Metadata'");
+    }
+
+    const [ctxTraceId, ctxParentId, ctxSpanId] = [
+      grpcMetadataFromIncomingCtx.get('x-b3-traceid')[0],
+      grpcMetadataFromIncomingCtx.get('x-b3-parentspanid')[0],
+      grpcMetadataFromIncomingCtx.get('x-b3-spanid')[0]
+    ];
+
+    if (!ctxTraceId || !ctxParentId || !ctxSpanId) {
+      throw new Error("Incoming X-B3 metadata corrupted, missing one of 'X-B3-TraceId', 'X-B3-ParentSpanId' or 'X-B3-SpanId'.");
+    }
+
+    let ctxSampled = grpcMetadataFromIncomingCtx.get('x-b3-sampled')[0];
+    if (!(ctxSampled in ['0', '1'])) {
+      ctxSampled = '0'
+    }
+
+    const ctxTraceInfo = new TraceId({
+      traceId: new Some(ctxTraceId),
+      parentId: new Some(ctxParentId),
+      spanId: ctxSpanId,
+      sampled: new Some(ctxSampled)
+    });
+
+    this.tracer.scoped(() => {
+      this.traceId = ctxTraceInfo;
+
+      this.tracer.setId(ctxTraceInfo);
+      this.tracer.recordServiceName(serviceName);
+
+      this.tracer.recordAnnotation(new Annotation.ServerRecv());
+      this.tracer.recordAnnotation(new Annotation.LocalAddr());
+    });
+  }
+
+  // Call this right before the GRPC server finish all respond.
+  uponServerFinishRespond() {
+    this.tracer.scoped(() => {
+      this.tracer.setId(this.traceId);
+      this.tracer.recordAnnotation(new Annotation.ServerSend());
+    });
+  }
+
+  // Call this right after everything of this GRPC call be finished at GRPC client side.
+  afterGrpcCallFinish() {
     this.tracer.scoped(() => {
       this.tracer.setId(this.traceId);
       this.tracer.recordAnnotation(new Annotation.ClientRecv());
