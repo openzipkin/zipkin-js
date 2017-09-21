99f3aad9604be52b5ff9dcad92d48701d6d52c00
Anton Shumsky <anton.shumsky@gmail.com>
2017-09-22 01:13:51 +0300

    add initial zipkin-instrumentation-koa package

---
+++ packages/zipkin-instrumentation-koa/src/zipkinMiddleware.js
@@ -0,0 +1,18 @@
+const {Annotation} = require('zipkin');
+
+module.exports = function zipkinMiddleware(tracer) {
+  return async (ctx, next) => {
+    const id = tracer.createRootId();
+    tracer.setId(id);
+    tracer.recordServiceName('ms-1');
+    tracer.recordRpc(ctx.request.method.toUpperCase());
+    tracer.recordBinary('http.url', ctx.request.path);
+    tracer.recordAnnotation(new Annotation.ServerRecv());
+    tracer.recordAnnotation(new Annotation.LocalAddr({port: '3001'}));
+    await next();
+
+    tracer.setId(id);
+    tracer.recordBinary('http.status_code', ctx.status.toString());
+    tracer.recordAnnotation(new Annotation.ServerSend());
+  };
+};
---
+++ packages/zipkin-instrumentation-koa/test/zipkinMiddlewareTest.js
@@ -0,0 +1,5 @@
+describe('zipkinMiddlewareTest', () => {
+  it('should work', () => {
+
+  });
+});
