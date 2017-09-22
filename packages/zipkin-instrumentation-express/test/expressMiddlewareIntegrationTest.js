a76130b1a99668684e3274939a04587ef852258a
dail <dail3513@gmail.com>
2017-09-23 00:39:48 +0800

    Update expressMiddlewareIntegrationTest.js

--- packages/zipkin-instrumentation-express/test/expressMiddlewareIntegrationTest.js
+++ packages/zipkin-instrumentation-express/test/expressMiddlewareIntegrationTest.js
@@ -1,5 +1,5 @@
 const sinon = require('sinon');
-const {Tracer, ExplicitContext} = require('zipkin');
+const { Tracer, ExplicitContext } = require('zipkin');
 const fetch = require('node-fetch');
 const express = require('express');
 const middleware = require('../src/expressMiddleware');
@@ -7,9 +7,9 @@ const middleware = require('../src/expressMiddleware');
 describe('express middleware - integration test', () => {
   it('should create traceId', done => {
     const record = sinon.spy();
-    const recorder = {record};
+    const recorder = { record };
     const ctxImpl = new ExplicitContext();
-    const tracer = new Tracer({recorder, ctxImpl});
+    const tracer = new Tracer({ recorder, ctxImpl });
 
     ctxImpl.scoped(() => {
       const app = express();
@@ -23,7 +23,7 @@ describe('express middleware - integration test', () => {
         setTimeout(() => {
           ctxImpl.letContext(ctx, () => {
             tracer.recordBinary('message', 'hello from within app');
-            res.status(202).json({status: 'OK'});
+            res.status(202).json({ status: 'OK' });
           });
         }, 10);
       });
@@ -81,9 +81,9 @@ describe('express middleware - integration test', () => {
   });
   it('should receive trace info from the client', done => {
     const record = sinon.spy();
-    const recorder = {record};
+    const recorder = { record };
     const ctxImpl = new ExplicitContext();
-    const tracer = new Tracer({recorder, ctxImpl});
+    const tracer = new Tracer({ recorder, ctxImpl });
 
     ctxImpl.scoped(() => {
       const app = express();
@@ -97,7 +97,7 @@ describe('express middleware - integration test', () => {
         setTimeout(() => {
           ctxImpl.letContext(ctx, () => {
             tracer.recordBinary('message', 'hello from within app');
-            res.status(202).json({status: 'OK'});
+            res.status(202).json({ status: 'OK' });
           });
         }, 10);
       });
@@ -159,9 +159,9 @@ describe('express middleware - integration test', () => {
 
   it('should properly report the URL with a query string', done => {
     const record = sinon.spy();
-    const recorder = {record};
+    const recorder = { record };
     const ctxImpl = new ExplicitContext();
-    const tracer = new Tracer({recorder, ctxImpl});
+    const tracer = new Tracer({ recorder, ctxImpl });
 
     ctxImpl.scoped(() => {
       const app = express();
@@ -175,7 +175,7 @@ describe('express middleware - integration test', () => {
         setTimeout(() => {
           ctxImpl.letContext(ctx, () => {
             tracer.recordBinary('message', 'hello from within app');
-            res.status(202).json({status: 'OK'});
+            res.status(202).json({ status: 'OK' });
           });
         }, 10);
       });
@@ -205,9 +205,9 @@ describe('express middleware - integration test', () => {
 
   it('should accept a 128bit X-B3-TraceId', done => {
     const record = sinon.spy();
-    const recorder = {record};
+    const recorder = { record };
     const ctxImpl = new ExplicitContext();
-    const tracer = new Tracer({recorder, ctxImpl});
+    const tracer = new Tracer({ recorder, ctxImpl });
 
     ctxImpl.scoped(() => {
       const app = express();
@@ -221,7 +221,7 @@ describe('express middleware - integration test', () => {
         setTimeout(() => {
           ctxImpl.letContext(ctx, () => {
             tracer.recordBinary('message', 'hello from within app');
-            res.status(202).json({status: 'OK'});
+            res.status(202).json({ status: 'OK' });
           });
         }, 10);
       });
@@ -252,4 +252,76 @@ describe('express middleware - integration test', () => {
       });
     });
   });
+
+  it('only contains flags header', done => {
+    const record = sinon.spy();
+    const recorder = { record };
+    const ctxImpl = new ExplicitContext();
+    const tracer = new Tracer({ recorder, ctxImpl });
+
+    ctxImpl.scoped(() => {
+      const app = express();
+      app.use(middleware({
+        tracer,
+        serviceName: 'service-a'
+      }));
+      app.post('/foo', (req, res) => {
+        // Use setTimeout to test that the trace context is propagated into the callback
+        const ctx = ctxImpl.getContext();
+        setTimeout(() => {
+          ctxImpl.letContext(ctx, () => {
+            tracer.recordBinary('message', 'hello from within app');
+            res.status(202).json({ status: 'OK' });
+          });
+        }, 10);
+      });
+      const server = app.listen(0, () => {
+        const port = server.address().port;
+        const url = `http://127.0.0.1:${port}/foo`;
+        fetch(url, {
+          method: 'post',
+          headers: {
+            'X-B3-Flags': '1'
+          }
+        }).then(res => res.json())
+          .then(() => {
+            server.close();
+
+            const annotations = record.args.map(args => args[0]);
+
+            expect(annotations[0].annotation.annotationType).to.equal('ServiceName');
+            expect(annotations[0].annotation.serviceName).to.equal('service-a');
+
+            expect(annotations[1].annotation.annotationType).to.equal('Rpc');
+            expect(annotations[1].annotation.name).to.equal('POST');
+
+            expect(annotations[2].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[2].annotation.key).to.equal('http.url');
+            expect(annotations[2].annotation.value).to.equal(url);
+
+            expect(annotations[3].annotation.annotationType).to.equal('ServerRecv');
+
+            expect(annotations[4].annotation.annotationType).to.equal('LocalAddr');
+
+            expect(annotations[5].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[5].annotation.key).to.equal('X-B3-Flags');
+
+            expect(annotations[6].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[6].annotation.key).to.equal('message');
+            expect(annotations[6].annotation.value).to.equal('hello from within app');
+
+            expect(annotations[7].annotation.annotationType).to.equal('BinaryAnnotation');
+            expect(annotations[7].annotation.key).to.equal('http.status_code');
+            expect(annotations[7].annotation.value).to.equal('202');
+
+            expect(annotations[8].annotation.annotationType).to.equal('ServerSend');
+            done();
+          })
+          .catch(err => {
+            server.close();
+            done(err);
+          });
+      });
+    });
+  })
 });
