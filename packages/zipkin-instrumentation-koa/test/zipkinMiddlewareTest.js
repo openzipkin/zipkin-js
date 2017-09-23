1460f042aeb4603db1cceb487e828899119cf460
Anton Shumsky <anton.shumsky@gmail.com>
2017-09-24 00:17:13 +0300

    use node-fetch instead of supertest

--- packages/zipkin-instrumentation-koa/test/zipkinMiddlewareTest.js
+++ packages/zipkin-instrumentation-koa/test/zipkinMiddlewareTest.js
@@ -3,12 +3,14 @@ const Koa = require('koa');
 
 const {Tracer, ExplicitContext} = require('zipkin');
 
-const request = require('supertest');
+const fetch = require('node-fetch');
 
 describe('zipkinMiddlewareTest', () => {
+  let app;
+  let server;
+  let tracer;
   let annotations;
   let binaryAnnotations;
-  let tracer;
 
   before(() => {
     const record = (rec) => {
@@ -22,33 +24,39 @@ describe('zipkinMiddlewareTest', () => {
   });
 
   beforeEach(() => {
+    app = new Koa();
     annotations = {};
     binaryAnnotations = {};
   });
 
+  afterEach(done => {
+    server.close(done);
+  });
+
   it('should record annotations for root span', (done) => {
-    const app = new Koa();
-    app.use(zipkinMiddleware({tracer, serviceName: 'dummy-service', port: 1234}));
+    app.use(zipkinMiddleware({tracer, serviceName: 'foo-service', port: 1234}));
     app.use(ctx => {
       ctx.status = 201;
     });
-    request(app.callback()).get('/test').end(() => {
-      expect(annotations['ServiceName']).not.to.be.undefined;
-      expect(annotations['ServiceName'].serviceName).to.be.equal('dummy-service');
+    server = app.listen(0, () => {
+      fetch(`http://localhost:${server.address().port}/foo`, {method: 'post'}).then(() => {
+        expect(annotations['ServiceName']).not.to.be.undefined;
+        expect(annotations['ServiceName'].serviceName).to.be.equal('foo-service');
 
-      expect(annotations['Rpc']).to.not.be.defined;
-      expect(annotations['Rpc'].name).to.not.be.equal('get');
+        expect(annotations['Rpc']).to.not.be.defined;
+        expect(annotations['Rpc'].name).to.not.be.equal('get');
 
-      expect(annotations['LocalAddr']).not.to.be.undefined;
-      expect(annotations['ServerRecv']).not.to.be.undefined;
-      expect(annotations['ServerSend']).not.to.be.undefined;
+        expect(annotations['LocalAddr']).not.to.be.undefined;
+        expect(annotations['ServerRecv']).not.to.be.undefined;
+        expect(annotations['ServerSend']).not.to.be.undefined;
 
-      expect(binaryAnnotations['http.url']).not.to.be.undefined;
-      expect(binaryAnnotations['http.url'].value).to.be.equal('/test');
+        expect(binaryAnnotations['http.url']).not.to.be.undefined;
+        expect(binaryAnnotations['http.url'].value).to.be.equal('/foo');
 
-      expect(binaryAnnotations['http.status_code']).not.to.be.undefined;
-      expect(binaryAnnotations['http.status_code'].value).to.be.equal('201');
-      done();
+        expect(binaryAnnotations['http.status_code']).not.to.be.undefined;
+        expect(binaryAnnotations['http.status_code'].value).to.be.equal('201');
+      }).then(done)
+        .catch(done);
     });
   });
 });
