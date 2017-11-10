3a24206e004a774735b5a5e9ed5648e945890ac4
Yonathan Randolph <yonathan@gmail.com>
2017-11-09 22:03:23 -0800

    TypeScript declarations for zipkin-instrumentation-request

---
+++ packages/zipkin-instrumentation-request/index.d.ts
@@ -0,0 +1,25 @@
+import {Tracer} from "zipkin"
+import {CoreOptions, Request, RequestAPI} from "request"
+
+/**
+ * Wraps the request (or request-promise) api with HttpClient instrumentation
+ *
+ * Note: you may have to explicitly provide the generic types from request
+ * or request-promise to make the TypeScript compiler happy e.g., for request,
+ *
+ *     tracingRequest = wrapRequest<
+ *        request.Request, request.CoreOptions, request.RequiredUriUrl
+ *        >(request, {tracer})
+ *
+ * or, for request-promise,
+ *
+ *     tracingRequest = wrapRequest<
+ *        rp.RequestPromise, rp.RequestPromiseOptions, request.RequiredUriUrl
+ *        >(rp, {tracer})
+ */
+declare function wrapRequest<TRequest extends Request, TOptions extends CoreOptions, TUriUrlOptions>(
+  request: RequestAPI<TRequest, TOptions, TUriUrlOptions>,
+  options: {tracer: Tracer, serviceName?: string, remoteServiceName?: string}
+): RequestAPI<TRequest, TOptions, TUriUrlOptions>;
+
+export = wrapRequest
