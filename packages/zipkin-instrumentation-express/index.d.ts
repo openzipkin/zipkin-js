1e76ca2ffde41efc9696a916aa6d022af8a9795b
Yonathan Randolph <yonathan@gmail.com>
2017-11-09 22:02:52 -0800

    TypeScript declarations for zipkin-instrumentation-express

---
+++ packages/zipkin-instrumentation-express/index.d.ts
@@ -0,0 +1,22 @@
+import {Tracer} from "zipkin"
+import {Handler} from "express"
+
+/**
+ * When a request comes in, creates ServerRecv annotation and then passes
+ * the request to the next middlewhare. When the final middleware finishes
+ * the request, creates ServerSend annotation
+ *
+ * Sets the tracer.id to the span id prior to running the next middleware.
+ *
+ * Note that if the next middleware makes async calls, it should either
+ * store the span id manually or use a CLSContext so that the annotations
+ * go to the correct spans
+ */
+export declare function expressMiddleware(
+  options: {tracer: Tracer, serviceName?: string, port?: number}
+): Handler;
+
+export declare function wrapExpressHttpProxy(
+  proxy: (host: string, options?: any) => Handler,
+  options: {tracer: Tracer, serviceName?: string, remoteServiceName?: string}
+): (host: string, options?: any) => Handler
