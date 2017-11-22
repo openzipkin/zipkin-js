f84b9c962fedd55a27e936c03545809ba7c67072
Chris Montoro <christopher.montoro@nytimes.com>
2017-11-22 01:07:19 -0500

    add support for optional fetch timeout

--- packages/zipkin-transport-http/index.d.ts
+++ packages/zipkin-transport-http/index.d.ts
@@ -1,7 +1,7 @@
 import {JsonEncoder, Logger, model} from "zipkin"
 
 declare class HttpLogger implements Logger {
-  constructor(options: {endpoint: string, httpInterval?: number, jsonEncoder?: JsonEncoder});
+  constructor(options: {endpoint: string, httpInterval?: number, jsonEncoder?: JsonEncoder, httpTimeout?: number});
   logSpan(span: model.Span): void;
 }
 export {HttpLogger}
