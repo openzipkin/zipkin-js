cbc6f074b77939fb838ae20849af6934d94bf231
Ben Keith <bkeith@signalfx.com>
2018-07-12 12:10:57 -0400

    Relax response code expectation to allow 200s
    We have mimicked the Zipkin API but don't use 202s since they are not appropriate

--- packages/zipkin-transport-http/src/HttpLogger.js
+++ packages/zipkin-transport-http/src/HttpLogger.js
@@ -60,7 +60,7 @@ class HttpLogger extends EventEmitter {
         headers: self.headers,
         timeout: self.timeout,
       }).then((response) => {
-        if (response.status !== 202) {
+        if (response.status !== 202 && response.status !== 200) {
           const err = 'Unexpected response while sending Zipkin data, status:' +
             `${response.status}, body: ${postBody}`;
 
