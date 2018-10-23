0c91bf5b218c4875a0e639ca4f23d0a5f4c6ccc9
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2018-10-22 23:14:09 -0300

    Removing network-address dep
    Added code to get ipv4 address and removed network-address dependency

---
+++ packages/zipkin/src/network.js
@@ -0,0 +1,21 @@
+const os = require('os');
+
+function pickInterface(interfaces, family) {
+  for (const i in interfaces) {
+    for (let j = interfaces[i].length - 1; j >= 0; j--) {
+      const face = interfaces[i][j];
+      const reachable = family === 'IPv4' || face.scopeid === 0;
+      if (!face.internal && face.family === family && reachable) return face.address;
+    }
+  }
+  return family === 'IPv4' ? '127.0.0.1' : '::1';
+}
+
+function ipv4() {
+  const interfaces = os.networkInterfaces();
+  return pickInterface(interfaces, 'IPv4');
+}
+
+module.exports = {
+  ipv4
+};
