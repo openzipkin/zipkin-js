c463175c4d2307eaaf6b4cff105847066a6fab42
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2018-10-23 00:29:29 -0300

    added other functions from network-address

--- packages/zipkin/src/network.js
+++ packages/zipkin/src/network.js
@@ -1,23 +1,39 @@
-const os = require('os');
+var os = require('os')
 
-function pickInterface(interfaces, family) {
+function pickInterface (interfaces, family) {
   /*eslint-disable */
-  for (const i in interfaces) {
+  for (var i in interfaces) {
     /*eslint-enable */
-    for (let j = interfaces[i].length - 1; j >= 0; j--) {
-      const face = interfaces[i][j];
-      const reachable = family === 'IPv4' || face.scopeid === 0;
-      if (!face.internal && face.family === family && reachable) return face.address;
+    for (var j = interfaces[i].length - 1; j >= 0; j--) {
+      var face = interfaces[i][j]
+      var reachable = family === 'IPv4' || face.scopeid === 0
+      if (!face.internal && face.family === family && reachable) return face.address
     }
   }
-  return family === 'IPv4' ? '127.0.0.1' : '::1';
+  return family === 'IPv4' ? '127.0.0.1' : '::1'
 }
 
-function ipv4() {
-  const interfaces = os.networkInterfaces();
-  return pickInterface(interfaces, 'IPv4');
+function reduceInterfaces (interfaces, iface) {
+  var ifaces = {}
+  for (var i in interfaces) {
+    if (i === iface) ifaces[i] = interfaces[i]
+  }
+  return ifaces
 }
 
-module.exports = {
-  ipv4
-};
+function ipv4 (iface) {
+  var interfaces = os.networkInterfaces()
+  if (iface) interfaces = reduceInterfaces(interfaces, iface)
+  return pickInterface(interfaces, 'IPv4')
+}
+
+function ipv6 (iface) {
+  var interfaces = os.networkInterfaces()
+  if (iface) interfaces = reduceInterfaces(interfaces, iface)
+  return pickInterface(interfaces, 'IPv6')
+}
+
+ipv4.ipv4 = ipv4
+ipv4.ipv6 = ipv6
+
+module.exports = ipv4
