e216bea3edf11f5af7f70e51e6285e2887b68654
Baiyuan Wang <wangbaiyuan@outlook.com>
2019-04-24 17:17:44 +0800

    #348 fix zipkin-instrumentation-axios; pass traceId from request to response by config

--- packages/zipkin-instrumentation-axios/src/index.js
+++ packages/zipkin-instrumentation-axios/src/index.js
@@ -1,28 +1,42 @@
 import {Instrumentation} from 'zipkin';
-const zipkinRecordError = (error, options) => {
-  const instrumentation = new Instrumentation.HttpClient(options);
-  if (error.response) {
-    instrumentation.recordResponse(options.tracer.id, error.response.status);
-  } else {
-    instrumentation.recordError(options.tracer.id, error);
-  }
-  return Promise.reject(error);
-};
-const zipkinRecordRequest = (config, options) => {
-  const instrumentation = new Instrumentation.HttpClient(options);
-  return instrumentation.recordRequest(config, config.url, config.method);
-};
-const zipkinRecordResponse = (res, options) => {
-  const instrumentation = new Instrumentation.HttpClient(options);
-  instrumentation.recordResponse(options.tracer.id, res.status);
-  return res;
-};
 
-const wrapAxios = (axios, options = {}) => {
-  axios.interceptors.request.use(config => zipkinRecordRequest(config, options),
-      err => zipkinRecordError(err, options));
-  axios.interceptors.response.use(res => zipkinRecordResponse(res, options),
-      err => zipkinRecordError(err, options));
-  return axios;
+export const wrapAxios = (axios, options = {}) => {
+  const {tracer} = options;
+  const instrumentation = new Instrumentation.HttpClient(options);
+  const zipkinRecordError = error => {
+    const traceId = error.config.traceId;
+    if (error.response) {
+      instrumentation.recordResponse(traceId, error.response.status);
+    } else {
+      instrumentation.recordError(traceId, error);
+    }
+    return Promise.reject(error);
+  };
+  const zipkinRecordRequest = config => {
+    const newConfig = instrumentation.recordRequest(
+      config,
+      config.url,
+      config.method
+    );
+    newConfig.traceId = tracer.id;
+    return newConfig;
+  };
+  const zipkinRecordResponse = res => {
+    instrumentation.recordResponse(res.config.traceId, res.status);
+    return res;
+  };
+  let axiosInstance = axios;
+  if (axios.create) {
+    axiosInstance = axios.create();
+  }
+  axiosInstance.interceptors.request.use(
+    config => zipkinRecordRequest(config),
+    err => zipkinRecordError(err)
+  );
+  axiosInstance.interceptors.response.use(
+    res => zipkinRecordResponse(res),
+    err => zipkinRecordError(err)
+  );
+  return axiosInstance;
 };
 export default wrapAxios;
