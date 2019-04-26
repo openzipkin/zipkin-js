35a8af4c1d2db028aeb676fe984bfc654c64af29
Baiyuan Wang <bywang@thoughtworks.com>
2019-04-26 15:49:32 +0800

    [Geekeren] #348 Add test for multiple requests

--- packages/zipkin-instrumentation-axios/src/index.js
+++ packages/zipkin-instrumentation-axios/src/index.js
@@ -3,7 +3,20 @@ import {Instrumentation} from 'zipkin';
 export const wrapAxios = (axios, options = {}) => {
   const {tracer} = options;
   const instrumentation = new Instrumentation.HttpClient(options);
-  const zipkinRecordError = error => {
+  const zipkinRecordRequest = config => tracer.scoped(() => {
+    const newConfig = instrumentation.recordRequest(
+        config,
+        config.url,
+        config.method
+      );
+    newConfig.traceId = tracer.id;
+    return newConfig;
+  });
+  const zipkinRecordResponse = res => tracer.scoped(() => {
+    instrumentation.recordResponse(res.config.traceId, res.status);
+    return res;
+  });
+  const zipkinRecordError = error => tracer.scoped(() => {
     const traceId = error.config.traceId;
     if (error.response) {
       instrumentation.recordResponse(traceId, error.response.status);
@@ -11,20 +24,7 @@ export const wrapAxios = (axios, options = {}) => {
       instrumentation.recordError(traceId, error);
     }
     return Promise.reject(error);
-  };
-  const zipkinRecordRequest = config => {
-    const newConfig = instrumentation.recordRequest(
-      config,
-      config.url,
-      config.method
-    );
-    newConfig.traceId = tracer.id;
-    return newConfig;
-  };
-  const zipkinRecordResponse = res => {
-    instrumentation.recordResponse(res.config.traceId, res.status);
-    return res;
-  };
+  });
   let axiosInstance = axios;
   if (axios.create) {
     axiosInstance = axios.create();
