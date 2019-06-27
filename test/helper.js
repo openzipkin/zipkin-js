8585d41a37d256bfafe4ac236e77f9107f2e646a
Adrian Cole <adriancole@users.noreply.github.com>
2019-06-27 18:23:17 +0800

    Shows object diff when an async assertion fails (#370)
    Before, the assertion failure that mocha could readily interpret was
    wrapped when inside an async hook. This made for very not helpful
    failures.
    
    This unwraps to prevent this.
    
    See https://github.com/mochajs/mocha/issues/2797

--- test/helper.js
+++ test/helper.js
@@ -3,6 +3,12 @@ const chai = require('chai');
 chai.config.includeStack = true;
 global.expect = chai.expect;
 
+// Otherwise assertion failures in async tests are wrapped, which prevents mocha from
+// being able to interpret them (such as displaying a diff).
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
 if (process.env.TEST_SUITE === 'browser') {
   /* eslint-disable no-console */
   console.log('WARN: Skipping nodejs tests because TEST_SUITE is \'browser\'.');
