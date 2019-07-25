d67b5d94a7fd47a54b1a711791a1e3efc68bac4b
Adrian Cole <acole@pivotal.io>
2019-07-25 20:01:21 +0800

    Pulls tracer up from request tests

--- packages/zipkin-instrumentation-request/test/integrationTest.js
+++ packages/zipkin-instrumentation-request/test/integrationTest.js
@@ -1,40 +1,17 @@
-const {expect} = require('chai');
-const {ExplicitContext, Tracer} = require('zipkin');
-
 const request = require('request');
-const {
-  expectB3Headers,
-  expectSpan,
-  newSpanRecorder,
-  setupTestServer
-} = require('../../../test/testFixture');
 const wrapRequest = require('../src/index');
+
+const {expectB3Headers, setupTestServer, setupTestTracer} = require('../../../test/testFixture');
 
 describe('request instrumentation - integration test', () => {
   const serviceName = 'weather-app';
   const remoteServiceName = 'weather-api';
 
   const server = setupTestServer();
-
-  let spans;
-  let tracer;
-
-  beforeEach(() => {
-    spans = [];
-    tracer = new Tracer({
-      ctxImpl: new ExplicitContext(),
-      localServiceName: serviceName,
-      recorder: newSpanRecorder(spans)
-    });
-  });
-
-  function popSpan() {
-    expect(spans).to.not.be.empty; // eslint-disable-line no-unused-expressions
-    return spans.pop();
-  }
+  const tracer = setupTestTracer({localServiceName: serviceName});
 
   function getClient() {
-    return wrapRequest(request, {tracer, remoteServiceName});
+    return wrapRequest(request, {tracer: tracer.tracer(), remoteServiceName});
   }
 
   function successSpan(path) {
@@ -52,78 +29,62 @@ describe('request instrumentation - integration test', () => {
 
   it('should add headers to requests', () => {
     const path = '/weather/wuhan';
-    return getClient().get(server.url(path))
-      .on('body', body => expectB3Headers(popSpan(), body));
+    getClient().get(server.url(path)).on('body', body => expectB3Headers(tracer.popSpan(), body));
   });
 
   it('should support get request', () => {
     const path = '/weather/wuhan';
-    return getClient().get(server.url(path), () => expectSpan(popSpan(), successSpan(path)));
+    getClient().get(server.url(path), () => tracer.expectNextSpanToEqual(successSpan(path)));
   });
 
   it('should support options request', () => {
     const path = '/weather/wuhan';
-    return getClient()({url: server.url(path)}, () => expectSpan(popSpan(), successSpan(path)));
+    getClient()({url: server.url(path)}, () => tracer.expectNextSpanToEqual(successSpan(path)));
   });
 
-  it('should report 404 in tags', (done) => {
+  it('should report 404 in tags', () => {
     const path = '/pathno';
-    getClient().get(server.url(path))
-      .on('response', () => {
-        expectSpan(popSpan(), {
-          name: 'get',
-          kind: 'CLIENT',
-          localEndpoint: {serviceName},
-          remoteEndpoint: {serviceName: remoteServiceName},
-          tags: {
-            'http.path': path,
-            'http.status_code': '404',
-            error: '404'
-          }
-        });
-        done();
-      })
-      .on('error', error => done(error));
+    getClient().get(server.url(path)).on('response', () => tracer.expectNextSpanToEqual({
+      name: 'get',
+      kind: 'CLIENT',
+      localEndpoint: {serviceName},
+      remoteEndpoint: {serviceName: remoteServiceName},
+      tags: {
+        'http.path': path,
+        'http.status_code': '404',
+        error: '404'
+      }
+    }));
   });
 
-  it('should report 401 in tags', (done) => {
+  it('should report 401 in tags', () => {
     const path = '/weather/securedTown';
-    getClient().get(server.url(path))
-      .on('response', () => {
-        expectSpan(popSpan(), {
-          name: 'get',
-          kind: 'CLIENT',
-          localEndpoint: {serviceName},
-          remoteEndpoint: {serviceName: remoteServiceName},
-          tags: {
-            'http.path': path,
-            'http.status_code': '401',
-            error: '401'
-          }
-        });
-        done();
-      })
-      .on('error', error => done(error));
+    getClient().get(server.url(path)).on('response', () => tracer.expectNextSpanToEqual({
+      name: 'get',
+      kind: 'CLIENT',
+      localEndpoint: {serviceName},
+      remoteEndpoint: {serviceName: remoteServiceName},
+      tags: {
+        'http.path': path,
+        'http.status_code': '401',
+        error: '401'
+      }
+    }));
   });
 
-  it('should report 500 in tags', (done) => {
+  it('should report 500 in tags', () => {
     const path = '/weather/bagCity';
-    getClient().get(server.url(path))
-      .on('response', () => {
-        expectSpan(popSpan(), {
-          name: 'get',
-          kind: 'CLIENT',
-          localEndpoint: {serviceName},
-          remoteEndpoint: {serviceName: remoteServiceName},
-          tags: {
-            'http.path': path,
-            'http.status_code': '500',
-            error: '500'
-          }
-        });
-        done();
-      })
-      .on('error', error => done(error));
+    getClient().get(server.url(path)).on('response', () => tracer.expectNextSpanToEqual({
+      name: 'get',
+      kind: 'CLIENT',
+      localEndpoint: {serviceName},
+      remoteEndpoint: {serviceName: remoteServiceName},
+      tags: {
+        'http.path': path,
+        'http.status_code': '500',
+        error: '500'
+      }
+    }));
   });
 
   it('should report when endpoint doesnt exist in tags', (done) => {
@@ -131,7 +92,7 @@ describe('request instrumentation - integration test', () => {
     const badUrl = `http://localhost:12345${path}`;
     getClient().get({url: badUrl, timeout: 300})
       .on('error', (error) => {
-        expectSpan(popSpan(), {
+        tracer.expectNextSpanToEqual({
           name: 'get',
           kind: 'CLIENT',
           localEndpoint: {serviceName},
@@ -153,9 +114,9 @@ describe('request instrumentation - integration test', () => {
     const wuhan = '/weather/wuhan';
 
     client.get(server.url(beijing), () => client.get(server.url(wuhan), () => {
-      // since these are sequential, we should have an expected order
-      expectSpan(popSpan(), successSpan(wuhan));
-      expectSpan(popSpan(), successSpan(beijing));
+      // since these are blocking requests, we should have an expected order
+      tracer.expectNextSpanToEqual(successSpan(wuhan));
+      tracer.expectNextSpanToEqual(successSpan(beijing));
     }));
   });
 });
