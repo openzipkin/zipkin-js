6768b0a53abd858c3fa3148aa4adadafe74c3da3
Adrian Cole <acole@pivotal.io>
2019-07-27 20:18:53 +0800

    Ports express tests

--- packages/zipkin-instrumentation-express/test/testMiddleware.js
+++ packages/zipkin-instrumentation-express/test/testMiddleware.js
@@ -1,16 +1,3 @@
-// Until there is a CLS hooked implementation here, we need to be explicit with trace IDs.
-// See https://github.com/openzipkin/zipkin-js/issues/88
-function step(tracer, req, num) {
-  return new Promise((done) => {
-    setTimeout(() => {
-      tracer.letId(req._trace_id, () => {
-        tracer.recordBinary('step', num.toString());
-        done();
-      });
-    }, 10);
-  });
-}
-
 function addTestRoutes(app, tracer) {
   app.get('/weather/wuhan', (req, res) => {
     if (tracer) tracer.recordBinary('city', 'wuhan');
@@ -20,18 +7,24 @@ function addTestRoutes(app, tracer) {
     if (tracer) tracer.recordBinary('city', 'beijing');
     res.status(200).json(req.headers);
   });
+  app.get('/weather/peking', (req, res) => {
+    if (tracer) tracer.recordBinary('city', 'peking');
+    res.redirect('/weather/beijing');
+  });
+  app.get('/weather/shenzhen', (req, res) => new Promise(done => setTimeout(() => {
+    tracer.letId(req._trace_id, () => {
+      tracer.recordBinary('city', 'shenzhen');
+      done();
+    });
+  }, 10)).then(() => res.send(200)));
   app.get('/weather/securedTown', (req, res) => {
     if (tracer) tracer.recordBinary('city', 'securedTown');
-    res.status(401).json(req.headers);
+    res.send(401);
   });
-  app.get('/weather/bagCity', (req, res, next) => {
+  app.get('/weather/bagCity', () => {
     if (tracer) tracer.recordBinary('city', 'bagCity');
-    next(new Error('service is dead'));
+    throw new Error('service is dead');
   });
-  app.get('/steps', (req, res) => step(tracer, req, 1)
-    .then(() => step(tracer, req, 2))
-    .then(() => step(tracer, req, 3))
-    .then(() => res.status(200).json(req.headers)));
   return app;
 }
 module.exports = addTestRoutes;
