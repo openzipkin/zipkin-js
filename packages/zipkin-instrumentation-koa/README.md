42c97de71cdae54733f75aaef1faacce8496b857
Anton Shumsky <ashumsky@conductor.com>
2019-07-31 17:12:11 +0300

    improve readme; replace async/await with promises

--- packages/zipkin-instrumentation-koa/README.md
+++ packages/zipkin-instrumentation-koa/README.md
@@ -1,6 +1,6 @@
 # zipkin-instrumentation-koa
 
-Zipkin middleware for Koa 2.x that traces incoming HTTP requests.
+Zipkin middleware for Koa 2.7+ that traces incoming HTTP requests.
 
 ## Installation
 `npm install zipkin-instrumentation-koa`
@@ -11,10 +11,11 @@ const Koa = require('koa');
 const {Tracer, ConsoleRecorder, ExplicitContext} = require('zipkin');
 const {zipkinMiddleware} = require('zipkin-instrumentation-koa');
 
-const recorder = new ConsoleRecorder();
 const ctxImpl = new ExplicitContext();
-const tracer = new Tracer({recorder, ctxImpl});
+const recorder = new ConsoleRecorder();
+const tracer = new Tracer({recorder, ctxImpl, serviceName: 'zipkin-koa-demo'});
+
 const app = new Koa();
 
-app.use(zipkinMiddleware({tracer, serviceName: 'zipkin-koa-demo', port: 3000}));
+app.use(zipkinMiddleware({tracer}));
 ```
--- packages/zipkin-instrumentation-koa/src/koaMiddleware.js
+++ packages/zipkin-instrumentation-koa/src/koaMiddleware.js
@@ -1,6 +1,6 @@
 const {option: {Some, None}, Instrumentation} = require('zipkin');
 
-module.exports = function zipkinMiddleware({tracer, serviceName = 'unknown', port = 0}) {
+module.exports = function zipkinMiddleware({tracer, serviceName, port = 0}) {
   const instrumentation = new Instrumentation.HttpServer({tracer, serviceName, port});
 
   return async function zipkinKoaMiddleware(ctx, next) {
@@ -12,17 +12,17 @@ module.exports = function zipkinMiddleware({tracer, serviceName = 'unknown', por
         return None;
       }
     }
-    tracer.scoped(async() => {
+    tracer.scoped(() => {
       const method = ctx.request.method.toUpperCase();
       const id = instrumentation.recordRequest(method, ctx.request.href, readHeader);
 
-      try {
-        await next();
-      } finally {
-        tracer.scoped(() => {
-          instrumentation.recordResponse(id, ctx.status);
-        });
-      }
+      const recordResponse = () => {
+        tracer.scoped(() => instrumentation.recordResponse(id, ctx.status));
+      };
+
+      next()
+        .then(recordResponse)
+        .catch(recordResponse);
     });
   };
 };
