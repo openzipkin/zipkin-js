2add96433a6f12881e902986af497ff08a8994bf
Robert Weber <RAWeber95@gmail.com>
2019-08-13 10:28:38 -0700

    Added type for http transport agent option

--- packages/zipkin-transport-http/index.d.ts
+++ packages/zipkin-transport-http/index.d.ts
@@ -1,4 +1,8 @@
 import {JsonEncoder, Logger, model} from 'zipkin';
+import {Agent as HttpAgent } from 'http';
+import {Agent as HttpsAgent} from 'https';
+
+type Agent = HttpAgent | HttpsAgent;
 
 declare class HttpLogger implements Logger {
   constructor(options: {
@@ -7,6 +11,7 @@ declare class HttpLogger implements Logger {
     jsonEncoder?: JsonEncoder,
     httpTimeout?: number,
     headers?: { [name: string]: any },
+    agent?: Agent | (() => Agent),
     log?: Console
   });
   logSpan(span: model.Span): void;
