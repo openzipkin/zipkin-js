8bb26e7f44eaac0b6cb0b67f22b8fc4074096f60
Robert Weber <RAWeber95@gmail.com>
2019-08-22 11:57:57 -0700

    Update documentation to reflect addition of http(s) Agent

--- packages/zipkin-transport-http/index.d.ts
+++ packages/zipkin-transport-http/index.d.ts
@@ -7,6 +7,18 @@ import {JsonEncoder, Logger, model} from 'zipkin';
 type Agent = HttpAgent | HttpsAgent;
 
 declare class HttpLogger implements Logger {
+  /**
+   * @constructor
+   * @param {Object} options
+   * @param {string} options.endpoint HTTP endpoint which spans will be sent
+   * @param {number} options.httpInterval How often to sync spans.
+   * @param {JsonEncoder} options.jsonEncoder JSON encoder to use when sending spans.
+   * @param {number} options.timeout Timeout for HTTP Post when sending spans.
+   * @param {number} options.maxPayloadSize Max payload size for zipkin span.
+   * @param {Object<string, any>} options.headers Any additional HTTP headers to be sent with span.
+   * @param {Agent|Function} options.agent HTTP(S) agent to use for any networking related options.
+   * @param {Console} options.log Internal logger used within the transport.
+   */
   constructor(options: {
     endpoint: string,
     httpInterval?: number,
