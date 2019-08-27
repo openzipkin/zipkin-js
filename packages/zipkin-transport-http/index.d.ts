db3314b92313c05c781e2990435307db3924f3d3
Robert Weber <RAWeber95@gmail.com>
2019-08-27 14:28:28 -0700

    Update http logger constructor types

--- packages/zipkin-transport-http/index.d.ts
+++ packages/zipkin-transport-http/index.d.ts
@@ -5,6 +5,9 @@ import {URL} from 'url';
 import {JsonEncoder, Logger, model} from 'zipkin';
 
 type Agent = HttpAgent | HttpsAgent;
+type ErrorLogger = {
+  error: Function
+};
 
 declare class HttpLogger implements Logger {
   /**
@@ -15,9 +18,9 @@ declare class HttpLogger implements Logger {
    * @param {JsonEncoder} options.jsonEncoder JSON encoder to use when sending spans.
    * @param {number} options.timeout Timeout for HTTP Post when sending spans.
    * @param {number} options.maxPayloadSize Max payload size for zipkin span.
-   * @param {Object<string, any>} options.headers Any additional HTTP headers to be sent with span.
+   * @param {Object<string, string>} options.headers Any additional HTTP headers to be sent with span.
    * @param {Agent|Function} options.agent HTTP(S) agent to use for any networking related options.
-   * @param {Console} options.log Internal logger used within the transport.
+   * @param {ErrorLogger} options.log Internal error logger used within the transport.
    */
   constructor(options: {
     endpoint: string,
@@ -25,9 +28,9 @@ declare class HttpLogger implements Logger {
     jsonEncoder?: JsonEncoder,
     timeout?: number,
     maxPayloadSize?: number,
-    headers?: { [name: string]: any },
+    headers?: { [name: string]: string },
     agent?: Agent | ((url: URL) => Agent),
-    log?: Console
+    log?: ErrorLogger
   });
 
   logSpan(span: model.Span): void;
--- packages/zipkin-transport-http/src/HttpLogger.js
+++ packages/zipkin-transport-http/src/HttpLogger.js
@@ -18,9 +18,9 @@ class HttpLogger extends EventEmitter {
    * @param {JsonEncoder} options.jsonEncoder JSON encoder to use when sending spans.
    * @param {number} options.timeout Timeout for HTTP Post when sending spans.
    * @param {number} options.maxPayloadSize Max payload size for zipkin span.
-   * @param {Object<string, any>} options.headers Any additional HTTP headers to be sent with span.
+   * @param {Object<string, string>} options.headers Any additional HTTP headers to be sent with span.
    * @param {Agent|Function} options.agent HTTP(S) agent to use for any networking related options.
-   * @param {Console} options.log Internal logger used within the transport.
+   * @param {ErrorLogger} options.log Internal error logger used within the transport.
    */
   constructor({
     endpoint,
