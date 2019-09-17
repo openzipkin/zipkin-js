bbd31696638bee9395bda137542c7a8a03569177
Arthur Gautier <baloo@gandi.net>
2019-09-17 06:55:47 +0000

    zipkin: traceid should be <2**63
    scribe defines trace_id as i64:
      https://github.com/openzipkin/zipkin-api/blob/master/thrift/zipkinCore.thrift#L431
    
    to my understanding, we should not interpret the 64bit hex-encoded as i64 either,
    I think it's safer to just generate id in the [0; 2**63[ range.
    
    Signed-off-by: Arthur Gautier <baloo@gandi.net>

--- packages/zipkin/src/tracer/randomTraceId.js
+++ packages/zipkin/src/tracer/randomTraceId.js
@@ -2,10 +2,13 @@
 function randomTraceId() {
   const digits = '0123456789abcdef';
   let n = '';
-  for (let i = 0; i < 16; i += 1) {
+  for (let i = 0; i < 15; i += 1) {
     const rand = Math.floor(Math.random() * 16);
     n += digits[rand];
   }
+  const rand = Math.floor(Math.random() * 8);
+  n = digits[rand] + n;
+
   return n;
 }
 
