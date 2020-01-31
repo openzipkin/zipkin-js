04bd88171acf0f48960f92e8147982982094db69
José Carlos Chávez <jcchavezs@gmail.com>
2020-01-31 13:08:50 +0100

    tests: makes the test suite run against both sync and async.

--- packages/zipkin-context-cls/test/CLSContext.test.js
+++ packages/zipkin-context-cls/test/CLSContext.test.js
@@ -1,69 +1,72 @@
 const CLSContext = require('../');
 
+const namespace = 'mynamespace'
+
 function CLSContextPerAsync(supportAsync) {
   it('should start with context null', () => {
-    const ctx = new CLSContext(supportAsync);
+    const ctx = new CLSContext(namespace, supportAsync);
     expect(ctx.getContext()).to.equal(null);
   });
 
   it('should set an inner context', () => {
-    const ctx = new CLSContext(supportAsync);
-    ctx.letContext('foo', () => {
-      expect(ctx.getContext()).to.equal('foo');
+    const ctx = new CLSContext(namespace, supportAsync);
+    ctx.letContext('tres-leches', () => {
+      expect(ctx.getContext()).to.equal('tres-leches');
     });
+    ctx.setContext(null)
   });
 
   it('should set an inner context with setContext', () => {
-    const ctx = new CLSContext(supportAsync);
+    const ctx = new CLSContext(namespace, supportAsync);
     ctx.scoped(() => {
-      ctx.setContext('bla');
-      expect(ctx.getContext()).to.equal('bla');
+      ctx.setContext('tiramisú');
+      expect(ctx.getContext()).to.equal('tiramisú');
     });
     expect(ctx.getContext()).to.equal(null);
   });
 
   it('should return the inner value', () => {
-    const ctx = new CLSContext(supportAsync);
-    const returnValue = ctx.letContext('foo', () => 123);
+    const ctx = new CLSContext(namespace, supportAsync);
+    const returnValue = ctx.letContext('cheesecake', () => 123);
     expect(returnValue).to.equal(123);
   });
 
   it('should be reset after the callback', () => {
-    const ctx = new CLSContext(supportAsync);
-    ctx.letContext('foo', () => {
+    const ctx = new CLSContext(namespace, supportAsync);
+    ctx.letContext('combinado', () => {
       // do nothing
     });
     expect(ctx.getContext()).to.equal(null);
   });
 
   it('should reset after error in callback', () => {
-    const ctx = new CLSContext(supportAsync);
+    const ctx = new CLSContext(namespace, supportAsync);
 
     let error;
     try {
-      ctx.letContext('buy-smoothie', () => {
-        throw new Error('no smoothies. try our cake');
+      ctx.letContext('buy-red-velvet', () => {
+        throw new Error('no red velvet. try our cake');
       });
     } catch (err) {
       error = err; // error wasn't swallowed
     }
 
     // sanity check
-    expect(error.message).to.eql('no smoothies. try our cake');
+    expect(error.message).to.eql('no red velvet. try our cake');
 
     // context wasn't leaked
     expect(ctx.getContext()).to.equal(null);
   });
 
   it('support nested contexts', () => {
-    const ctx = new CLSContext(supportAsync);
-    const finalReturnValue = ctx.letContext('foo', () => {
-      expect(ctx.getContext()).to.equal('foo');
-      const innerReturnValue = ctx.letContext('bar', () => {
-        expect(ctx.getContext()).to.equal('bar');
+    const ctx = new CLSContext(namespace, supportAsync);
+    const finalReturnValue = ctx.letContext('lemon-pie', () => {
+      expect(ctx.getContext()).to.equal('lemon-pie');
+      const innerReturnValue = ctx.letContext('pannacotta', () => {
+        expect(ctx.getContext()).to.equal('pannacotta');
         return 1;
       });
-      expect(ctx.getContext()).to.equal('foo');
+      expect(ctx.getContext()).to.equal('lemon-pie');
       return innerReturnValue + 2;
     });
     expect(ctx.getContext()).to.equal(null);
@@ -71,12 +74,13 @@ function CLSContextPerAsync(supportAsync) {
   });
 
   it('supports CLS contexts (setTimeout etc)', (done) => {
-    const ctx = new CLSContext(supportAsync);
+    const ctx = new CLSContext(namespace, supportAsync);
     function callback() {
-      expect(ctx.getContext()).to.equal('foo');
+      expect(ctx.getContext()).to.equal('brownie');
+      ctx.setContext(null)
       done();
     }
-    ctx.letContext('foo', () => {
+    ctx.letContext('brownie', () => {
       setTimeout(callback, 10);
     });
   });
@@ -92,26 +96,24 @@ describe('CLSContext', () => {
     CLSContextPerAsync(true);
 
     it('support async-await contexts', async () => {
-      const ctx = new CLSContext('zipkin', true);
+      const ctx = new CLSContext(namespace, true);
   
       async function getCtx() {
         return ctx.getContext();
       }
   
       async function callback() {
-        const ctx1 = await getCtx(1);
-        const ctx2 = await getCtx(2);
-        return {ctx1, ctx2};
+        const ctx = await getCtx();
+        return ctx;
       }
   
-      async function promiseFor(id) {
-        const {c1, c2} = await ctx.letContext(id, () => callback());
-        console.log(c1)
-        console.log(c2)
-        expect(c1).to.not.equal(null);
-        expect(c2).to.not.equal(null);
+      async function fn() {
+        const ctx1 = await ctx.letContext("budin", () => callback());
+        const ctx2 = await ctx.letContext("torta-helada", () => callback());
+        expect(ctx1).to.equal("budin");
+        expect(ctx2).to.equal("torta-helada");
       }
-      await promiseFor(1); // eslint-disable-line
+      await fn(); // eslint-disable-line
     });
   })
 });
